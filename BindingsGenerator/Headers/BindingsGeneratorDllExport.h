704b472f3381702cfdbd12f32c3af6533e196005
Alan Wills <alawills@googlemail.com>
2020-06-14 11:01:08 +0100

    Removing unused packages

--- BindingsGenerator/Headers/BindingsGeneratorDllExport.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#ifdef BUILDING_BINDINGS_GENERATOR_DLL
-#define BindingsGeneratorDllExport __declspec(dllexport)
-#else
-#define BindingsGeneratorDllExport __declspec(dllimport)
-#endif
